4bb0d9e57f0c52e8c2f3e4874a80c36c3c2c4357
tealeaf2 <shadowrocket2003@gmail.com>
2024-02-17 00:38:17 -0500

    base

--- alora-backend/backend/settings.py
+++ alora-backend/backend/settings.py
@@ -15,8 +15,7 @@ from datetime import timedelta
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 
 
