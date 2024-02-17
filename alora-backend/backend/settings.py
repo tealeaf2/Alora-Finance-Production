5959f8ae4c6dd69a43c1adc0c165dc2280745bae
tealeaf2 <shadowrocket2003@gmail.com>
2024-02-17 04:08:28 -0500

    static root

--- alora-backend/backend/settings.py
+++ alora-backend/backend/settings.py
@@ -202,6 +202,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = 'static/icons'
+STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
